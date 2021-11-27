564c2049988f651e97e8ea8554bdafd740446ffe
AndreaLozanoCortes <alozanoc@unah.hn>
2021-11-27 10:40:39 -0600

    AndreaLozano
    Creación de Menú

---
+++ ProyectoFinal_Grupo2/Vista/MenuView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoFinal_Grupo2.Vista
+{
+    public partial class MenuView : Syncfusion.Windows.Forms.Office2010Form
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
