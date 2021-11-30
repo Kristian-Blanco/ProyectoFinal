7dee5e4800dd0cf5b068127887e3540018dac255
Elvin Hernández <94201127+ElvinAlejandro@users.noreply.github.com>
2021-11-30 10:34:56 -0600

    Update ProductoDAO.cs

--- ProyectoFinal_Grupo2/Modelos/DAO/ProductoDAO.cs
+++ ProyectoFinal_Grupo2/Modelos/DAO/ProductoDAO.cs
@@ -1,13 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProyectoFinal_Grupo2.Modelos.DAO
 {
-    public class ProductoDAO
+    public class ProductoDAO : Conexion
     {
+        SqlCommand comando = new SqlCommand();
+        
+        public DataTable GetProductos()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT * FROM PRODUCTO ");
+
+                comando.Connection = MiConexion;
+                MiConexion.Open();
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = sql.ToString();
+                SqlDataReader dr = comando.ExecuteReader();
+                dt.Load(dr);
+                MiConexion.Close();
+            }
+            catch (Exception)
+            {
+            }
+            return dt;
+        }
+
+        public DataTable GetProductosPorCodigo(string codigo)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT * FROM PRODUCTO WHERE NOMBRE LIKE ('%" + codigo + "%') ");
+
+                comando.Connection = MiConexion;
+                MiConexion.Open();
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = sql.ToString();
+                SqlDataReader dr = comando.ExecuteReader();
+                dt.Load(dr);
+                MiConexion.Close();
+            }
+            catch (Exception)
+            {
+                MiConexion.Close();
+            }
+            return dt;
+        }
+
 
     }
 }
