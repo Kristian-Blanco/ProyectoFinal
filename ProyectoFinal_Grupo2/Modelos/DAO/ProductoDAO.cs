7b3956af4f0294ff73a1e9cf9f7c005f88bf047f
Elvin Hernández <94201127+ElvinAlejandro@users.noreply.github.com>
2021-11-30 10:09:24 -0600

    Creación de Clase ProductoDAO

---
+++ ProyectoFinal_Grupo2/Modelos/DAO/ProductoDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Grupo2.Modelos.DAO
+{
+    public class ProductoDAO
+    {
+
+    }
+}
