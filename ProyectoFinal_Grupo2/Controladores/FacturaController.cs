2b238fd5a05b391f1887c05b6fb402a1ab15b946
Pilar-Mendoza <pilygonzales2018@gmail.com>
2021-12-02 11:47:22 -0600

    Factura

--- ProyectoFinal_Grupo2/Controladores/FacturaController.cs
+++ ProyectoFinal_Grupo2/Controladores/FacturaController.cs
@@ -40,3 +40,34 @@ namespace ProyectoFinal_Grupo2.Controladores
 
     }
 }
+
+public class FacturaController
+{
+    FacturaView vista;
+    Producto producto = new Producto();
+    ProductoDAO productoDAO = new ProductoDAO();
+
+    public FacturaController(FacturaView view)
+    {
+        vista = view;
+        vista.CodigoProductoTextBox_KeyPress;
+    }
+    private void CodigoProductoTextBox_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == (char)Keys.Enter)
+        {
+            producto = productoDAO.GetProductoPorCodigo(vista.CodigoProductoTextBox.Text);
+
+            vista.DescripcionProductoTextBox.Text = producto.Descripcion;
+        }
+        else
+        {
+            producto = null;
+            vista.DescripcionProductoTextBox.Clear();
+        }
+    }
+
+
+}
+}
+
